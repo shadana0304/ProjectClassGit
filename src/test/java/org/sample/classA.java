63466aef1ad4e9ec4d8cc96fcd797675e6964731
SaranyaRagu <saranya3606@gmail.com>
2022-11-10 11:38:57 +0530

    First time push

--- src/test/java/org/sample/classA.java
+++ src/test/java/org/sample/classA.java
@@ -1,5 +1,14 @@
 package org.sample;
 
 public class classA {
+public void Saran() {
+	System.out.println("saran");
+}
+public void Ragu() {
+	System.out.println("ragu");
+}
+public void iniyan() {
+	System.out.println("iniyan");
 
+}
 }
